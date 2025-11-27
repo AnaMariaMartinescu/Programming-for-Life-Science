20af4abab7028c3cae6a614931a105e0b37974b0
Ana-Maria Martinescu <ana.martinescu100@gmail.com>
2025-11-27 21:29:16 +0100

    Delete Ratify directory

--- Ratify/utils.js
+++
@@ -1,7 +0,0 @@
-export function make(tag, props = {}, parent) {
-  const el = document.createElement(tag);
-  Object.assign(el, props);
-  if (parent) parent.appendChild(el);
-  return el;
-}
-export const eh = s => String(s ?? "").replace(/[&<>"']/g, c => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
